53effb2428b92517d1e3a82c6221bbaf2ea20574
Taylor Austin <tbaustin1992@gmail.com>
2021-10-07 20:22:55 -0500

    template functions and overloading template functions

--- other/swap.cpp
+++ other/swap.cpp
@@ -1,21 +1,33 @@
 #include <iostream>
 #include <string>
 
+template <typename T>
 // & passes by reference so you keep and can modify the original value
-void swap(int &a, int &b)
+void swap(T &a, T &b)
 {
 
-  int temp = a;
+  T temp = a;
   a = b;
   b = temp;
 }
 
-void swap(std::string &a, std::string &b)
+template <typename T>
+void swap(T a[], T b[], int size)
 {
-  std::string temp = a;
-  a = b;
-  b = temp;
+  for (int i = 0; i < size; i++)
+  {
+    T temp = a[i];
+    a[i] = b[i];
+    b[i] = temp;
+  }
 }
+
+// void swap(std::string &a, std::string &b)
+// {
+//   std::string temp = a;
+//   a = b;
+//   b = temp;
+// }
 
 int main()
 {
@@ -24,9 +36,29 @@ int main()
 
   swap(a, b);
 
+  std::cout << a << " " << b << std::endl;
+
   std::string first_name = "Taylor";
   std::string last_name = "Austin";
 
   swap(first_name, last_name);
   std::cout << first_name << " " << last_name << std::endl;
+
+  int nines[] = {9, 9, 9, 9};
+  int ones[] = {1, 1, 1, 1};
+  int const arraySize = 4;
+
+  for (int i = 0; i < arraySize; i++)
+  {
+    std::cout << nines[i] << " " << ones[i] << "\t";
+  }
+  std::cout << "\n\n";
+
+  swap(nines, ones, arraySize);
+
+  for (int i = 0; i < arraySize; i++)
+  {
+    std::cout << nines[i] << " " << ones[i] << "\t";
+  }
+  std::cout << "\n\n";
 }
